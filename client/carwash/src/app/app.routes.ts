12f237ae613e0986df582101354813436742efb7
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-02 16:23:33 +0200

    added routes for home , gallery , services

--- client/carwash/src/app/app.routes.ts
+++ client/carwash/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ServiceComponent } from './pages/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'service', component:ServiceComponent}
+
+];
